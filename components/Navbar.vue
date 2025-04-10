f405bc73df831f42c1228febbc7eca58cae6afda
sahdanhm <sahdanhusna057@gmail.com>
2025-04-10 15:53:50 +0700

    Fixing navbar and add dropdown language

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,21 +1,40 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 
 const isMenuOpen = ref(false);
-
+const url = useRequestURL();
 const toggleMenu = () => {
   isMenuOpen.value = !isMenuOpen.value;
 };
+const handleScroll = () => {
+  const navBar = document.getElementById('navbar');
+  const scrollTop =
+    document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (scrollTop >= 20) {
+    navBar.style.backgroundColor = 'white';
+    navBar.style.borderBottom = '1px solid rgba(0, 0, 0, 0.1)';
+  } else {
+    navBar.style.backgroundColor = 'transparent';
+    navBar.style.borderBottom = 'none';
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
+});
 </script>
 <template>
   <header
     class="sticky top-0 flex flex-wrap sm:justify-start sm:flex-nowrap w-full py-3 z-100"
     id="navbar"
   >
-    <nav
-      class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
-    >
-      <div class="flex items-center justify-between">
+    <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center">
+      <div class="flex items-center justify-between sm:me-auto">
         <a
           class="flex-none text-2xl focus:opacity-80 alexandria-bold"
           href="/"
@@ -23,6 +42,9 @@ const toggleMenu = () => {
         >
           syahdan
         </a>
+        <div class="sm:hidden">
+          <DropdownLanguage />
+        </div>
         <div class="sm:hidden">
           <button
             @click="toggleMenu"
@@ -33,11 +55,6 @@ const toggleMenu = () => {
           >
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
-            <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
             <svg
               :class="{ hidden: isMenuOpen }"
               id="i-menu"
@@ -55,11 +72,6 @@ const toggleMenu = () => {
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
               />
             </svg>
-            <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
             <svg
               :class="{ hidden: !isMenuOpen }"
               id="i-x"
@@ -83,156 +95,62 @@ const toggleMenu = () => {
       <div
         :class="{ hidden: !isMenuOpen }"
         id="navbar-menu"
-        class="overflow-hidden transition-all duration-300 basis-full grow sm:block absolute sm:relative "
+        class="sm:block absolute sm:relative right-3 left-3 sm:right-0 sm:left-0 mt-5 sm:mt-0 px-5 pb-5 sm:px-0 sm:pb-0 rounded-2xl bg-white sm:bg-transparent shadow-2xl sm:shadow-none"
         aria-labelledby="navbar-menu"
       >
         <div
-          class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5"
+          class="flex flex-col mt-5 sm:gap-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0"
         >
           <a
-            class="nav-link font-medium color1-text"
+            :class="{
+              'color1-text': url.pathname === '/',
+              'transition-[padding] sm:transition-transform duration-300 active:duration-100 ease-in-out active:ps-10 hover:ps-10 sm:active:ps-0 sm:hover:scale-150 sm:active:scale-150 sm:hover:ps-0':
+                url.pathname !== '/',
+            }"
+            class="font-medium py-2 sm:py-0"
             href="/"
-            aria-current="page"
             >Home</a
           >
-          <a class="nav-link font-medium" href="/works">Works</a>
-          <a class="nav-link font-medium" href="/contact">Contact</a>
-          <a class="nav-link font-medium" href="/about-me">About me</a>
-          <div
-            class="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none]"
+          <a
+            :class="{
+              'color1-text': url.pathname === '/works',
+              'transition-[padding] sm:transition-transform duration-300 active:duration-100 ease-in-out active:ps-10 hover:ps-10 sm:active:ps-0 sm:hover:scale-150 sm:active:scale-150 sm:hover:ps-0':
+                url.pathname !== '/works',
+            }"
+            class="font-medium py-2 sm:py-0"
+            href="/works"
+            >Works</a
           >
-            <button
-              id="hs-navbar-example-dropdown"
-              type="button"
-              class="hs-dropdown-toggle flex items-center w-full text-gray-600 hover:text-gray-400 focus:outline-hidden focus:text-gray-400 font-medium dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
-              aria-haspopup="menu"
-              aria-expanded="false"
-              aria-label="Mega Menu"
-            >
-              Dropdown
-              <svg
-                class="hs-dropdown-open:-rotate-180 sm:hs-dropdown-open:rotate-0 duration-300 ms-1 shrink-0 size-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="m6 9 6 6 6-6" />
-              </svg>
-            </button>
-
-            <div
-              class="hs-dropdown-menu transition-[opacity,margin] ease-in-out duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 z-10 bg-white sm:shadow-md rounded-lg p-1 space-y-1 dark:bg-neutral-800 sm:dark:border dark:border-neutral-700 dark:divide-neutral-700 before:absolute top-full sm:border border-gray-200 before:-top-5 before:start-0 before:w-full before:h-5 hidden"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="hs-navbar-example-dropdown"
-            >
-              <a
-                class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                href="#"
-              >
-                About
-              </a>
-              <div
-                class="hs-dropdown [--strategy:static] sm:[--strategy:absolute] [--adaptive:none] relative"
-              >
-                <button
-                  id="hs-navbar-example-dropdown-sub"
-                  type="button"
-                  class="hs-dropdown-toggle w-full flex justify-between items-center text-sm text-gray-800 rounded-lg py-2 px-3 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                >
-                  Sub Menu
-                  <svg
-                    class="hs-dropdown-open:-rotate-180 sm:hs-dropdown-open:-rotate-90 sm:-rotate-90 duration-300 ms-2 shrink-0 size-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="m6 9 6 6 6-6" />
-                  </svg>
-                </button>
-
-                <div
-                  class="hs-dropdown-menu transition-[opacity,margin] ease-in-out duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 hidden z-10 sm:mt-2 bg-white sm:shadow-md rounded-lg dark:bg-neutral-800 sm:dark:border dark:border-neutral-700 dark:divide-neutral-700 before:absolute sm:border border-gray-200 before:-end-5 before:top-0 before:h-full before:w-5 sm:mx-2.5! top-0 end-full"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="hs-navbar-example-dropdown-sub"
-                >
-                  <div class="p-1 space-y-1">
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                      href="#"
-                    >
-                      About
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                      href="#"
-                    >
-                      Downloads
-                    </a>
-                    <a
-                      class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                      href="#"
-                    >
-                      Team Account
-                    </a>
-                  </div>
-                </div>
-              </div>
-              <a
-                class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                href="#"
-              >
-                Downloads
-              </a>
-              <a
-                class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300"
-                href="#"
-              >
-                Team Account
-              </a>
-            </div>
-          </div>
+          <a
+            :class="{
+              'color1-text': url.pathname === '/contact',
+              'transition-[padding] sm:transition-transform duration-300 active:duration-100 ease-in-out active:ps-10 hover:ps-10 sm:active:ps-0 sm:hover:scale-150 sm:active:scale-150 sm:hover:ps-0':
+                url.pathname !== '/contact',
+            }"
+            class="font-medium py-2 sm:py-0"
+            href="/contact"
+            >Contact</a
+          >
+          <a
+            :class="{
+              'color1-text': url.pathname === '/about-me',
+              'transition-[padding] sm:transition-transform duration-300 active:duration-100 ease-in-out active:ps-10 hover:ps-10 sm:active:ps-0 sm:hover:scale-150 sm:active:scale-150 sm:hover:ps-0':
+                url.pathname !== '/about-me',
+            }"
+            class="font-medium py-2 sm:py-0"
+            href="/about-me"
+            >About me</a
+          >
         </div>
+      </div>
+      <div class="hidden sm:block mx-10">
+        <DropdownLanguage />
       </div>
     </nav>
   </header>
 </template>
 <style>
-.nav-link {
-  transition: transform 0.3s ease-in-out;
-}
-
-.nav-link::after {
-  content: '';
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  width: 0;
-  height: 2px;
-  background-color: black;
-  transform: translateY(-50%);
-  transition: width 0.3s ease-in-out, left 0.3s ease-in-out;
-}
-
-.nav-link:hover {
-  transform: scale(1.2);
-}
-
-.nav-link:hover::after {
-  width: 100%;
-  left: 0;
+#navbar {
+  transition: background-color 0.2s ease;
 }
 </style>
